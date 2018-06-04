964948eb8554fbf28f87b1434da8cfb15c3089e1
jeddyrakoto <jeddy@wau.solutions>
2018-06-04 16:32:15 +0300

    fix treating treating matches to treat only not generated yet

--- dist/utils/index.js
+++ dist/utils/index.js
@@ -20,9 +20,9 @@ var _replaceTxtNotInA = exports._replaceTxtNotInA = function _replaceTxtNotInA(h
             // Add http:// to link that does not have it or only add <a> balise
             matches.forEach(function (match) {
                 if (match.includes('http') !== true) {
-                    txt = txt.replace(new RegExp("\\b" + match + "\\b"), "<a href='" + "http:\/\/" + match + "' target='_blank'> " + match + "</a>");
+                    txt = txt.replace(new RegExp("\\b(?<!https?:\/\/)" + match.replace(/\./, "\\.") + "\\b"), "<a href='" + "http:\/\/" + match + "' target='_blank'> " + match + "</a>");
                 } else {
-                    txt = txt.replace(new RegExp("\\b" + match + "\\b"), "<a href='" + match + "' target='_blank'>" + match + "</a>");
+                    txt = txt.replace(new RegExp("\\b(?<!https?:\/\/)" + match.replace(/\./, "\\.") + "\\b"), "<a href='" + match + "' target='_blank'>" + match + "</a>");
                 }
             });
         }
